cfaf5617385455ca2ca7711e4914d1ff2c082a55
HuynhTuan <huynhthanhtuan2206@gmail.com>
2023-11-19 22:44:52 +0700

    Complete system server v3.0

--- server/springboot/src/main/java/com/medical/springboot/SpringbootApplication.java
+++ server/springboot/src/main/java/com/medical/springboot/SpringbootApplication.java
@@ -3,10 +3,12 @@ package com.medical.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import com.medical.springboot.configs.RsaKeyConfig;
 
 @EnableConfigurationProperties(RsaKeyConfig.class)
+@EnableScheduling
 @SpringBootApplication
 public class SpringbootApplication {
 
