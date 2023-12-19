1738887cba652007dae4cff5cd592ce29be652e0
NguyenDat27 <caitlyn.ochamp@gmail.com>
2023-12-19 20:32:28 +0700

    fixbug medical record detail (admin, doctor)

--- client/src/components/Form/Admin/MedicalRecord/MedicalRecordDetailAdmin.jsx
+++ client/src/components/Form/Admin/MedicalRecord/MedicalRecordDetailAdmin.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { readMedicalRecordDetailAdminApi } from '../../../../api'
 import axios from 'axios'
 import moment from 'moment'
@@ -10,6 +10,7 @@ const MedicalRecordDetailAdmin = () => {
   const { id } = useParams();
   const [medicalRecord, setMedicalRecord] = useState(null);
   const token = localStorage.getItem("token");
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchMedicalRecordDetail = async () => {
